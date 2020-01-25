edbf0539a891090aefe17fa502dc28c8068316df
Yudai-8-om <53668296+Yudai-8-om@users.noreply.github.com>
2020-01-25 15:51:04 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,12 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1wRP_KPnEkagTSmovZW_dlnZYpcM6GZXB'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Nike', 'Adidas', 'Tommy Hilfiger', 'Ralph Lauren', 'Lacoste', 
+		   'Gucci', 'Versace', 'Under Armour', 'Hollister', 'Levi Strauss',
+		   'Supreme', 'Burberry', 'Abercrombie & Fitch']
 path = Path(__file__).parent
 
 app = Starlette()
